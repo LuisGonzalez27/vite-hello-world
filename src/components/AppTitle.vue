6bd7481305ac2cdb93a2075b6f39bb63fc3e398d
LuisGonzalez27 <luis27coding@gmail.com>
2022-11-09 15:18:56 +0100

    Componente AppTittle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'AppTitle',
+    data() {
+        return {
+            titolo: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <h1>{{ titolo }}</h1>
+    </header>
+</template>
+
+<style lang="css">
+header {
+    width: 100%;
+    height: 100px;
+    background-color: black;
+    padding: 20px;
+}
+
+h1 {
+    text-align: center;
+    color: white;
+    text-transform: uppercase;
+}
+</style>
