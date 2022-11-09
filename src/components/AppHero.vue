9288a0289bae1dbf750a27efe9e15b7ed246e81f
LuisGonzalez27 <luis27coding@gmail.com>
2022-11-09 16:36:09 +0100

    Componente AppHero

---
+++ src/components/AppHero.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    name: 'AppHero',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="jumbotron">
+        <div class="jumbotron-text">
+            <h2>I am Vite</h2>
+        </div>
+    </div>
+</template>
+
+<style lang="css">
+.jumbotron {
+    width: 100%;
+    height: 80vh;
+    background-image: url(./images/vite.jpg);
+    background-position: center;
+    background-repeat: no-repeat;
+    position: relative;
+}
+
+.jumbotron-text {
+    position: absolute;
+    text-align: center;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: black;
+    font-size: 50px;
+}
+</style>
